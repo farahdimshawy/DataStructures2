a16e429356d059f02d001c633e33802f444ab0e3
Farah <148490880+farahdimshawy@users.noreply.github.com>
2024-05-18 20:45:34 +0300

    added exit in main menu

--- main.cpp
+++ main.cpp
@@ -256,137 +256,142 @@ void minHeapMenu() {
         }
     } while (choice != 0);
 }
-int main() {   
-    cout << "Choose a non-linear data structure..\n";
-    cout << "1. Binary Search Tree\n";
-    cout << "2. AVL Tree\n";
-    cout << "4. Max Heap\n";
-    cout << "5. Min Heap\n";
-    int type;
-    cin >> type;
-    switch (type) {
-        case 1:{ 
-            BST nameTree, priceTree(false);
-            cout << "Choose one of the following choices..\n";
-            miniMenu();
-            int choice;
-            while (cin >> choice, choice != 0) {
-                switch (choice) {
-                    case 1: {
-                        string name, category;
-                        double price;
-                        cout << "Enter item name, category, price (space separated)\n";
-                        cin >> name >> category >> price;
-                        nameTree.insert(new Item(name, category, price));
-                        priceTree.insert(new Item(name, category, price));
-                        break;
-                    }
-                    case 2: {
-                        ifstream inputFile("items.txt");
-                        if(!inputFile){
-                            cerr << "Error opening file\n";
-                        }
-                        readItems(inputFile, nameTree);
-                        inputFile.close();
-                        inputFile.open("items.txt");
-                        readItems(inputFile, priceTree);
-                        break;
-                    }
-                    case 3: {
-                        cout << "Enter name of the item you want to delete\n";
-                        string name;
-                        cin >> name;
-                        nameTree.Delete(name);
-                        priceTree.Delete(name);
-                        break;
-                    }
-                    case 4:
-                        nameTree.preorder(nameTree.getRoot());
-                        break;
-                    case 5:
-                        nameTree.inorderASC(nameTree.getRoot());
-                        break;
-                    case 6:
-                        nameTree.inorderDESC(nameTree.getRoot());
-                        break;
-                    case 7:
-                        priceTree.inorderASC(priceTree.getRoot());
-                        break;
-                    case 8:
-                        priceTree.inorderDESC(priceTree.getRoot());
-                        break;
-                    default:
-                        cout << "Invalid choice\n";
-                }
+int main() {
+    while(true) {
+        cout << "Choose a non-linear data structure..\n";
+        cout << "1. Binary Search Tree\n";
+        cout << "2. AVL Tree\n";
+        cout << "4. Max Heap\n";
+        cout << "5. Min Heap\n";
+        cout << "0. Exit\n";
+        int type;
+        cin >> type;
+        switch (type) {
+            case 1: {
+                BST nameTree, priceTree(false);
+                cout << "Choose one of the following choices..\n";
                 miniMenu();
-            }
-            break;
-        }
-        case 2:{
-            PriceAVLTree priceTree;
-            NameAVLTree nameTree;
-            cout << "Choose one of the following choices..\n";
-            miniMenu();
-            int choice;
-            while (cin >> choice, choice != 0) {
-                switch (choice) {
-                    case 1: {
-                        string name, category;
-                        double price;
-                        cout << "Enter item name, category, price (space separated)\n";
-                        cin >> name >> category >> price;
-                        Item item(name, category, price);
-                        nameTree.insertNodeAVL(item);
-                        priceTree.insertNodeAVL(item);
-                        break;
+                int choice;
+                while (cin >> choice, choice != 0) {
+                    switch (choice) {
+                        case 1: {
+                            string name, category;
+                            double price;
+                            cout << "Enter item name, category, price (space separated)\n";
+                            cin >> name >> category >> price;
+                            nameTree.insert(new Item(name, category, price));
+                            priceTree.insert(new Item(name, category, price));
+                            break;
+                        }
+                        case 2: {
+                            ifstream inputFile("items.txt");
+                            if (!inputFile) {
+                                cerr << "Error opening file\n";
+                            }
+                            readItems(inputFile, nameTree);
+                            inputFile.close();
+                            inputFile.open("items.txt");
+                            readItems(inputFile, priceTree);
+                            break;
+                        }
+                        case 3: {
+                            cout << "Enter name of the item you want to delete\n";
+                            string name;
+                            cin >> name;
+                            nameTree.Delete(name);
+                            priceTree.Delete(name);
+                            break;
+                        }
+                        case 4:
+                            nameTree.preorder(nameTree.getRoot());
+                            break;
+                        case 5:
+                            nameTree.inorderASC(nameTree.getRoot());
+                            break;
+                        case 6:
+                            nameTree.inorderDESC(nameTree.getRoot());
+                            break;
+                        case 7:
+                            priceTree.inorderASC(priceTree.getRoot());
+                            break;
+                        case 8:
+                            priceTree.inorderDESC(priceTree.getRoot());
+                            break;
+                        default:
+                            cout << "Invalid choice\n";
                     }
-                    case 2: {
-                        vector<Item> items = readFile("items.txt");
-                        for(auto item : items){
+                    miniMenu();
+                }
+                break;
+            }
+            case 2: {
+                PriceAVLTree priceTree;
+                NameAVLTree nameTree;
+                cout << "Choose one of the following choices..\n";
+                miniMenu();
+                int choice;
+                while (cin >> choice, choice != 0) {
+                    switch (choice) {
+                        case 1: {
+                            string name, category;
+                            double price;
+                            cout << "Enter item name, category, price (space separated)\n";
+                            cin >> name >> category >> price;
+                            Item item(name, category, price);
                             nameTree.insertNodeAVL(item);
                             priceTree.insertNodeAVL(item);
+                            break;
                         }
-                        break;
+                        case 2: {
+                            vector<Item> items = readFile("items.txt");
+                            for (auto item: items) {
+                                nameTree.insertNodeAVL(item);
+                                priceTree.insertNodeAVL(item);
+                            }
+                            break;
+                        }
+                        case 3: {
+                            cout << "Enter name of the item you want to delete\n";
+                            string name;
+                            cin >> name;
+                            Item item = nameTree.Search(name);
+                            nameTree.deleteItem(item);
+                            priceTree.deleteItem(item);
+                            break;
+                        }
+                        case 4:
+                            nameTree.preorder(nameTree.getRoot());
+                            break;
+                        case 5:
+                            nameTree.Inorder(nameTree.getRoot());
+                            break;
+                        case 6:
+                            nameTree.InorderDesc(nameTree.getRoot());
+                            break;
+                        case 7:
+                            priceTree.Inorder(priceTree.getRoot());
+                            break;
+                        case 8:
+                            priceTree.InorderDesc(priceTree.getRoot());
+                            break;
+                        default:
+                            cout << "Invalid choice\n";
                     }
-                    case 3: {
-                        cout << "Enter name of the item you want to delete\n";
-                        string name;
-                        cin >> name;
-                        Item item = nameTree.Search(name);
-                        nameTree.deleteItem(item);
-                        priceTree.deleteItem(item);
-                        break;
-                    }
-                    case 4:
-                        nameTree.preorder(nameTree.getRoot());
-                        break;
-                    case 5:
-                        nameTree.Inorder(nameTree.getRoot());
-                        break;
-                    case 6:
-                        nameTree.InorderDesc(nameTree.getRoot());
-                        break;
-                    case 7:
-                        priceTree.Inorder(priceTree.getRoot());
-                        break;
-                    case 8:
-                        priceTree.InorderDesc(priceTree.getRoot());
-                        break;
-                    default:
-                        cout << "Invalid choice\n";
+                    miniMenu();
                 }
-                miniMenu();
+                break;
             }
-            break;
+            case 4:
+                maxHeapMenu();
+                break;
+            case 5:
+                minHeapMenu();
+                break;
+            case 0:
+                cout << "Exiting...";
+                return 0;
+            default:
+                cout << "Invalid type\n";
         }
-        case 4:
-            maxHeapMenu();
-            break;
-        case 5:
-            minHeapMenu();
-            break;
-        default:
-            cout << "Invalid type\n";
     }
-
 }
